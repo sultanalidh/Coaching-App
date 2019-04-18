0c331275c7d6cb72bffe4432e84526e86a596490
sultanalidh <sultanalidh@gmail.com>
2019-04-17 22:12:43 -0600

    Update importscript.php

--- importscript.php
+++ importscript.php
@@ -1,59 +1,84 @@
-<?php /* Template Name: Importig1 */
+<?php /* Template Name: ImportScriptline */ 
+
+
+
 
 /* Here is the function to get input from the add scripts form. It will have the name
 of the scripts and the Author's name.  */
-
 get_header();
 ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<?php do_action( 'ocean_before_content_wrap' ); ?>
 
-			<?php
+	<div id="content-wrap" class="container clr">
 
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+		<?php do_action( 'ocean_before_primary' ); ?>
 
-				get_template_part( 'template-parts/content/content', 'page' );
+		<div id="primary" class="content-area clr">
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
+			<?php do_action( 'ocean_before_content' ); ?>
 
-			endwhile; // End of the loop.
-			?>
+			<div id="content" class="site-content clr">
 
-		</main><!-- #main -->
-    </section><!-- #primary -->
-    
+				<?php do_action( 'ocean_before_content_inner' ); ?>
+
+				<?php
+				// Elementor `single` location
+				if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_location( 'single' ) ) {
+					
+					// Start loop
+					while ( have_posts() ) : the_post();
+
+						get_template_part( 'partials/page/layout' );
+
+					endwhile;
+
+				} ?>
+
+				<?php do_action( 'ocean_after_content_inner' ); ?>
+
+			</div><!-- #content -->
+
+			<?php do_action( 'ocean_after_content' ); ?>
+
+		</div><!-- #primary -->
+
+		<?php do_action( 'ocean_after_primary' ); ?>
+
+	</div><!-- #content-wrap -->
+
+	<?php do_action( 'ocean_after_content_wrap' ); ?>
 <?php
-/* Attempt MySQL server connection. */
-$link = mysqli_connect("localhost", "sultan_wrdp2", "12345", "sultan_wrdp2");
- 
+
+
+
+	
+/* Attempt MySQL server connection.  */
+$link = mysqli_connect("localhost", "*****", "*****!","*****");
+
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-// Escape user inputs for security
-
-$scriptName = mysqli_real_escape_string($link, $_REQUEST['scriptName']);
-$Author = mysqli_real_escape_string($link, $_REQUEST['Author']);
-
- 
 // Attempt insert query execution
-$sql = "INSERT INTO ca_Scripts (scriptName, Author) VALUES
-            ('$scriptName', '$Author')";
+if ( ! empty(isset($_POST['scriptName'])) && ( ! empty(isset($_POST['Author']))))  {
+    $scriptName = $_POST['scriptName'];
+    $Author = $_POST['Author'];
+
+	$sql = "INSERT INTO ca_Scripts (scriptName, scriptAuthor) VALUES
+		    ('$_POST[scriptName]','$_POST[Author]')";
 if(mysqli_multi_query($link, $sql)){
-    echo "Records added successfully.";
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
- 
-if (isset($_POST["import"])) {
     
+} else{
+    echo "ERROR: Could not able to execute $sql. " .
+	    mysqli_error($link);
+}
+ //print_r($_POST);
+ //echo "Here ". (isset($_POST["import"]));
+if ( ! empty(isset($_POST["import"])))  {
+    
+}
     $fileName = $_FILES["file"]["tmp_name"];
     
     if ($_FILES["file"]["size"] > 0) {
@@ -66,8 +91,7 @@ if (isset($_POST["import"])) {
 			/* to import the data from a file to the data base and add it to the cs_Scripts table */
 			$sql = "INSERT into  ca_scriptLines (scriptLineId,scriptid , act, scene, scriptline, lineText, characterId)
 				   values ('" . $column[0] . "',(SELECT MAX(scriptId) FROM ca_Scripts) ,'" . $column[2] . "','" . $column[3] . "','" . $column[4] . "','" . $column[5] . "','" . $column[6] . "')";
-				  /* "SELECT scriptId FROM ca_Scripts WHERE scriptName = 'scriptName' ";*/
-				 /*  "SELECT MAX(scriptId) FROM ca_Scripts";*/
+				  
             $result = mysqli_multi_query($link, $sql); 
             
             
@@ -75,11 +99,15 @@ if (isset($_POST["import"])) {
     if (! empty($result)) {
             $type = "success";
             $message = "CSV Data Imported into the Database";
+            /* This will redierct to the Manging the scripts page after importing the file */
+            header('Location: http://jeremysortore.com/home/');
+            
     } 
     else {
         $type = "error";
         $message = "Problem in Importing CSV Data";
-            }
+    }
+            
         }
     }
 }
@@ -90,5 +118,8 @@ if (isset($_POST["import"])) {
 mysqli_close($link);
 
 
+?>
+
 <?php
 get_footer();
+
